184274b0f83a904e3c40feda5d1eb91269b9060f
Chanaka Abeysinghe <chanakasan@gmail.com>
2022-07-20 12:21:46 +0530

    omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :user_logins
+	devise_for :user_logins,
+		controllers: {
+		omniauth_callbacks: 'user_logins/omniauth_callbacks'
+	}
+
   root 'pages#home'
 end
